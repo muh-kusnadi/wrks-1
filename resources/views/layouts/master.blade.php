00fe6e1f0c5ade618c0b68d230099df6f82af3c9
kusnadi99 <muh.kusnadi99@gmail.com>
2020-01-28 15:07:00 +0800

    remove set active on menu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -46,7 +46,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="{{route('home')}}">Dashboard</a></li>
+            <li><a href="{{route('home')}}">Dashboard</a></li>
             <li><a href="#">Data Siswa</a></li>
           </ul>
         </div>
