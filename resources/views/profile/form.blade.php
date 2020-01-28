a94bab6c0a382c3aa20b1588ba09551a0bb3eb8f
kusnadi99 <muh.kusnadi99@gmail.com>
2020-01-28 13:02:47 +0800

    create controller and view for profile

---
+++ resources/views/profile/form.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('breadcrumbs')
+<section class="content-header">
+    <h1>
+      Profile
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+    </ol>
+</section>
+@endsection
+
+@section('content')
+
+@endsection
