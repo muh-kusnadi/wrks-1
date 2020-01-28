baedb5a4bd979cfcac9d5c3fc03e35db14990a14
kusnadi99 <muh.kusnadi99@gmail.com>
2020-01-28 12:52:49 +0800

    modify home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('breadcrumbs')
+<section class="content-header">
+    <h1>
+      Home
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    </ol>
+</section>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<div class="box box-default">
+    <div class="box-header with-border">
+      <h3 class="box-title">Blank Box</h3>
     </div>
+    <div class="box-body">
+      The great content goes here
+    </div>
+    <!-- /.box-body -->
 </div>
 @endsection
